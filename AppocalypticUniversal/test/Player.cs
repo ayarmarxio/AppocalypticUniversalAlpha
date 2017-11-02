0e0ed7a9975653c040a9f3d4856b6ff190623f69
Mario Andres Ravina <mari81dh@edu.easj.dk>
2017-11-02 14:25:57 +0100

    added folder with class for player stats

---
+++ AppocalypticUniversal/test/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppocalypticUniversal.test
+{
+    class Player
+    {
+        public int health { get; set; }
+    }
+}
