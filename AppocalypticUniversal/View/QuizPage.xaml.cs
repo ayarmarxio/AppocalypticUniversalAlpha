15479539e3034c5f97554e88990195f536726e74
Mario Andres Ravina <ayarmarxio@gmail.com>
2017-11-08 01:56:10 +0100

    Grid Fixing and TextBlock Size Adaptation updates. It is missing some minimizing responsive design. by Mario

--- AppocalypticUniversal/View/QuizPage.xaml.cs
+++ AppocalypticUniversal/View/QuizPage.xaml.cs
@@ -26,5 +26,10 @@ namespace AppocalypticUniversal
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
     }
 }
